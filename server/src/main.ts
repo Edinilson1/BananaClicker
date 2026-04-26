e689c8b1467439f69e69b8921bd1df6459e74815
Edinilson <edinilsonsantossilvafilho@outlook.com>
2026-04-25 21:41:01 -0300

    feat: Nestjs initialization

---
+++ server/src/main.ts
@@ -0,0 +1,34 @@
+import { config } from 'dotenv';
+config();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(cookieParser());
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+  app.enableShutdownHooks();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      disableErrorMessages: process.env.NODE_ENV === 'production',
+    }),
+  );
+  
+  const port = process.env.PORT ?? 4000;
+  await app.listen(port, '0.0.0.0');
+}
+
+bootstrap();
